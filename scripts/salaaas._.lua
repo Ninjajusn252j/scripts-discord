86dc772acdd27c85dca3439c791257cff6ef6007
Ninjajusn252j <juanleonardo375@gmail.com>
2025-06-06 21:06:44 +0200

    feat: Add/Update script for salaaas._

---
+++ scripts/salaaas._.lua
@@ -0,0 +1,4 @@
+-- Script Lua generado por DarkEclipse252j para salaaas._
+local GroupLink = "https://is.gd/ER8NW3"
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Ninjajusn252j/universal-hub/main/Universalhub.lua"))()
