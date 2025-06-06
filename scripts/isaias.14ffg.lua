54479eaf8b8f81f0fcefabeaca66da94d1119b88
Ninjajusn252j <juanleonardo375@gmail.com>
2025-06-07 01:08:16 +0200

    feat: Add/Update script for isaias.14ffg

---
+++ scripts/isaias.14ffg.lua
@@ -0,0 +1,6 @@
+-- Script Lua generado por DarkEclipse252j para isaias.14ffg
+-- Script Principal - Lo que ejecutas en tu executor
+_G.Config = {
+    GroupLink = "https://is.gd/Bk0S1e)", -- <--- ¡Esta línea ahora tiene la URL real desde Python!
+}
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Ninjajusn252j/universal-hub/main/Universalhub.lua"))()
