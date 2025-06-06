34de2fb4e40de2b4a89a343580bd4ca038b4c9a7
Ninjajusn252j <juanleonardo375@gmail.com>
2025-06-06 21:25:01 +0200

    feat: Add/Update script for gonzalo01034

--- scripts/gonzalo01034.lua
+++ scripts/gonzalo01034.lua
@@ -1,4 +1,6 @@
 -- Script Lua generado por DarkEclipse252j para gonzalo01034
-local GroupLink = "https://is.gd/BGjlrt"
-
+-- Script Principal - Lo que ejecutas en tu executor
+_G.Config = {
+    GroupLink = "https://is.gd/7LwywS", -- <--- ¡Esta línea ahora tiene la URL real desde Python!
+}
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Ninjajusn252j/universal-hub/main/Universalhub.lua"))()
