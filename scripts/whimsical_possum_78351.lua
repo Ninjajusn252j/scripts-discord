78e6e9fe78796e5b2cc1e77f4a2b11dc13212a6e
Ninjajusn252j <juanleonardo375@gmail.com>
2025-06-06 20:21:53 +0200

    feat: Add/Update script for whimsical_possum_78351

---
+++ scripts/whimsical_possum_78351.lua
@@ -0,0 +1,4 @@
+-- Script Lua generado por DarkEclipse252j para whimsical_possum_78351
+local GroupLink = "https://is.gd/M8ZqUH"
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Ninjajusn252j/universal-hub/main/Universalhub.lua"))()
