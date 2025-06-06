62043f3f5e2857821fb7dffbb5b53a6023271249
Ninjajusn252j <juanleonardo375@gmail.com>
2025-06-06 21:28:47 +0200

    Update alewzzz.lua

--- scripts/alewzzz.lua
+++ scripts/alewzzz.lua
@@ -1,4 +1,7 @@
 -- Script Lua generado por DarkEclipse252j para alewzzz
-local GroupLink = "https://is.gd/QyYFl7"
+
+_G.Config = {
+    GroupLink = "https://is.gd/QyYFl7", -- <--- ¡Esta línea ahora tiene la URL real desde Python!
+}
 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Ninjajusn252j/universal-hub/main/Universalhub.lua"))()
