47cf5a4178ddcec7e0cc0e71a6845309eb2f051f
Ninjajusn252j <juanleonardo375@gmail.com>
2025-06-06 21:30:37 +0200

    Update whimsical_possum_78351.lu

--- scripts/whimsical_possum_78351.lua
+++ scripts/whimsical_possum_78351.lua
@@ -1,4 +1,6 @@
 -- Script Lua generado por DarkEclipse252j para whimsical_possum_78351
 local GroupLink = "https://is.gd/M8ZqUH"
-
+_G.Config = {
+    GroupLink = "https://is.gd/M8ZqUH", -- <--- ¡Esta línea ahora tiene la URL real desde Python!
+}
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Ninjajusn252j/universal-hub/main/Universalhub.lua"))()
