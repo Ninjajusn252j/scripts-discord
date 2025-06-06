553f4a06df7d16e1267d4b18448ba97708df3773
Ninjajusn252j <juanleonardo375@gmail.com>
2025-06-06 20:19:26 +0200

    feat: Add/Update script for alewzzz

---
+++ scripts/alewzzz.lua
@@ -0,0 +1,4 @@
+-- Script Lua generado por DarkEclipse252j para alewzzz
+local GroupLink = "https://is.gd/QyYFl7"
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Ninjajusn252j/universal-hub/main/Universalhub.lua"))()
