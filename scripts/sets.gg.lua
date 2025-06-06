e82c1f31b5dbaffb43bf709a9db772cd27b59747
Ninjajusn252j <juanleonardo375@gmail.com>
2025-06-06 21:36:53 +0200

    feat: Add/Update script for sets.gg

--- scripts/sets.gg.lua
+++ scripts/sets.gg.lua
@@ -1,6 +1,6 @@
 -- Script Lua generado por DarkEclipse252j para sets.gg
 -- Script Principal - Lo que ejecutas en tu executor
 _G.Config = {
-    GroupLink = "https://is.gd/ZxVGDS", -- <--- ¡Esta línea ahora tiene la URL real desde Python!
+    GroupLink = "https://is.gd/fPbIYF", -- <--- ¡Esta línea ahora tiene la URL real desde Python!
 }
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Ninjajusn252j/universal-hub/main/Universalhub.lua"))()
