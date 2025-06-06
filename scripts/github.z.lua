56360659ed401d69bcf987da462b7c60a4a4e5cb
Ninjajusn252j <juanleonardo375@gmail.com>
2025-06-06 22:17:22 +0200

    feat: Add/Update script for github.z

---
+++ scripts/github.z.lua
@@ -0,0 +1,6 @@
+-- Script Lua generado por DarkEclipse252j para github.z
+-- Script Principal - Lo que ejecutas en tu executor
+_G.Config = {
+    GroupLink = "https://is.gd/M66mks", -- <--- ¡Esta línea ahora tiene la URL real desde Python!
+}
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Ninjajusn252j/universal-hub/main/Universalhub.lua"))()
