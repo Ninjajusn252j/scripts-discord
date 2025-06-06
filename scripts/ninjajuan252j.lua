1d80fcc7e6491843631df5a81f2c977731dcb47b
Ninjajusn252j <juanleonardo375@gmail.com>
2025-06-06 17:10:36 +0200

    feat: Add/Update script for ninjajuan252j

---
+++ scripts/ninjajuan252j.lua
@@ -0,0 +1,4 @@
+-- Script Lua generado por DarkEclipse252j para ninjajuan252j
+local GroupLink = "https://"
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Ninjajusn252j/universal-hub/main/Universalhub.lua"))()
