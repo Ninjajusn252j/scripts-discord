18287cbe62c6f13f888507749933c5df22c98a47
Ninjajusn252j <juanleonardo375@gmail.com>
2025-06-06 22:26:25 +0200

    Update github.z.lua

--- scripts/github.z.lua
+++ scripts/github.z.lua
@@ -1,6 +1,6 @@
--- Script Lua generado por DarkEclipse252j para github.z
+-- Script Lua generado por DarkEclipse252j para cristian013663
 -- Script Principal - Lo que ejecutas en tu executor
 _G.Config = {
-    GroupLink = "https://is.gd/M66mks", -- <--- ¡Esta línea ahora tiene la URL real desde Python!
+    GroupLink = "https://is.gd/Cu3VDZ", -- <--- ¡Esta línea ahora tiene la URL real desde Python!
 }
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Ninjajusn252j/universal-hub/main/Universalhub.lua"))()
