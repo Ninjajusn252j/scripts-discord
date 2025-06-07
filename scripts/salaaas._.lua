4f0e294a778db1902aeaa3c20b2f506311a0202f
Ninjajusn252j <juanleonardo375@gmail.com>
2025-06-07 03:52:12 +0200

    feat: Add/Update script for salaaas._

--- scripts/salaaas._.lua
+++ scripts/salaaas._.lua
@@ -1,6 +1,6 @@
 -- Script Lua generado por DarkEclipse252j para salaaas._
 -- Script Principal - Lo que ejecutas en tu executor
 _G.Config = {
-    GroupLink = "https://is.gd/C8s56Z", -- <--- ¡Esta línea ahora tiene la URL real desde Python!
+    GroupLink = "https://is.gd/JIZxCa", -- <--- ¡Esta línea ahora tiene la URL real desde Python!
 }
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Ninjajusn252j/universal-hub/main/Universalhub.lua"))()
