8344d1c07e9e06c28f1991faa79d64f53794f0b7
Ninjajusn252j <juanleonardo375@gmail.com>
2025-06-07 02:16:15 +0200

    feat: Add/Update script for jc_soru

---
+++ scripts/jc_soru.lua
@@ -0,0 +1,6 @@
+-- Script Lua generado por DarkEclipse252j para jc_soru
+-- Script Principal - Lo que ejecutas en tu executor
+_G.Config = {
+    GroupLink = "https://is.gd/z9NZ0P", -- <--- ¡Esta línea ahora tiene la URL real desde Python!
+}
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Ninjajusn252j/universal-hub/main/Universalhub.lua"))()
