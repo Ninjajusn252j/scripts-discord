1841fa02dd89be3454422785177275333e6bc292
Ninjajusn252j <juanleonardo375@gmail.com>
2025-06-08 14:31:48 +0200

    feat: Add/Update script for papu_an

--- scripts/papu_an.lua
+++ scripts/papu_an.lua
@@ -1,6 +1,6 @@
 -- Script Lua generado por DarkEclipse252j para papu_an
 -- Script Principal - Lo que ejecutas en tu executor
 _G.Config = {
-    GroupLink = "https://is.gd/iMJbFl", -- <--- ¡Esta línea ahora tiene la URL real desde Python!
+    GroupLink = "https://is.gd/QG5rQA", -- <--- ¡Esta línea ahora tiene la URL real desde Python!
 }
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Ninjajusn252j/universal-hub/main/Universalhub.lua"))()
