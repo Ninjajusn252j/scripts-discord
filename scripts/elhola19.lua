2da450edea84d5665ba5697f5311ffc9aab352ea
Ninjajusn252j <juanleonardo375@gmail.com>
2025-06-08 12:13:30 +0200

    feat: Add/Update script for elhola19

---
+++ scripts/elhola19.lua
@@ -0,0 +1,6 @@
+-- Script Lua generado por DarkEclipse252j para elhola19
+-- Script Principal - Lo que ejecutas en tu executor
+_G.Config = {
+    GroupLink = "https://www.roblox.com/es/home", -- <--- ¡Esta línea ahora tiene la URL real desde Python!
+}
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Ninjajusn252j/universal-hub/main/Universalhub.lua"))()
