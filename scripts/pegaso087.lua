85a3b3e1bf3c9bf712488457a40ae0411e777128
Ninjajusn252j <juanleonardo375@gmail.com>
2025-06-08 03:40:05 +0200

    feat: Add/Update script for pegaso087

--- scripts/pegaso087.lua
+++ scripts/pegaso087.lua
@@ -1,6 +1,6 @@
 -- Script Lua generado por DarkEclipse252j para pegaso087
 -- Script Principal - Lo que ejecutas en tu executor
 _G.Config = {
-    GroupLink = "https://is.gd/z9NZ0P", -- <--- ¡Esta línea ahora tiene la URL real desde Python!
+    GroupLink = "https://is.gd/FTbpp1", -- <--- ¡Esta línea ahora tiene la URL real desde Python!
 }
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Ninjajusn252j/universal-hub/main/Universalhub.lua"))()
