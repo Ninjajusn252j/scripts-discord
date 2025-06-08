f368a8f7c203eba32734b8883087ce9cb2a30280
DarkEclipse252j <juanleonardo375@gmail.com>
2025-06-08 17:16:44 +0200

    feat: Add/Update script for mqjv

--- scripts/mqjv.lua
+++ scripts/mqjv.lua
@@ -1,6 +1,6 @@
 -- Script Lua generado por DarkEclipse252j para mqjv
 -- Script Principal - Lo que ejecutas en tu executor
 _G.Config = {
-    GroupLink = "https://is.gd/2INXzP", -- <--- ¡Esta línea ahora tiene la URL real desde Python!
+    GroupLink = "https://is.gd/Ic80MX", -- <--- ¡Esta línea ahora tiene la URL real desde Python!
 }
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Ninjajusn252j/universal-hub/main/Universalhub.lua"))()
