92c9529d89a4416a404041572bb92bd78d0fbab4
Ninjajusn252j <juanleonardo375@gmail.com>
2025-06-08 04:39:02 +0200

    feat: Add/Update script for pingenstocksihaydough

--- scripts/pingenstocksihaydough.lua
+++ scripts/pingenstocksihaydough.lua
@@ -1,6 +1,6 @@
 -- Script Lua generado por DarkEclipse252j para pingenstocksihaydough
 -- Script Principal - Lo que ejecutas en tu executor
 _G.Config = {
-    GroupLink = "https://is.gd/gONtrl", -- <--- ¡Esta línea ahora tiene la URL real desde Python!
+    GroupLink = "https://is.gd/TOsw4V", -- <--- ¡Esta línea ahora tiene la URL real desde Python!
 }
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Ninjajusn252j/universal-hub/main/Universalhub.lua"))()
