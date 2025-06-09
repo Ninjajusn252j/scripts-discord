a041a66763e00590622fd93074dd3bded95be6dc
DarkEclipse252j <juanleonardo375@gmail.com>
2025-06-09 18:42:10 +0200

    feat: Add/Update script for shutth

--- scripts/shutth.lua
+++ scripts/shutth.lua
@@ -1,6 +1,6 @@
 -- Script Lua generado por DarkEclipse252j para shutth
 -- Script Principal - Lo que ejecutas en tu executor
 _G.Config = {
-    GroupLink = "https://is.gd/qL0mKy", -- <--- ¡Esta línea ahora tiene la URL real desde Python!
+    GroupLink = "https://is.gd/lHQj6z", -- <--- ¡Esta línea ahora tiene la URL real desde Python!
 }
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Ninjajusn252j/universal-hub/main/Universalhub.lua"))()
