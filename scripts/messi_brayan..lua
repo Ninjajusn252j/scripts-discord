2e683a956d999ac076992c7aec8a8833604e44f1
DarkEclipse252j <juanleonardo375@gmail.com>
2025-06-09 17:22:02 +0200

    feat: Add/Update script for messi_brayan.

--- scripts/messi_brayan..lua
+++ scripts/messi_brayan..lua
@@ -1,6 +1,6 @@
 -- Script Lua generado por DarkEclipse252j para messi_brayan.
 -- Script Principal - Lo que ejecutas en tu executor
 _G.Config = {
-    GroupLink = "https://is.gd/qiDBRv", -- <--- ¡Esta línea ahora tiene la URL real desde Python!
+    GroupLink = "https://is.gd/LtQSe", -- <--- ¡Esta línea ahora tiene la URL real desde Python!
 }
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Ninjajusn252j/universal-hub/main/Universalhub.lua"))()
