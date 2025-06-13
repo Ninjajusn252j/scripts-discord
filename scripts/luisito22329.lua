3c7e34bf8a96b0e67fff857633e4a49d06c64a9e
DarkEclipse252j <juanleonardo375@gmail.com>
2025-06-13 21:37:29 +0200

    feat: Add/Update script for luisito22329

---
+++ scripts/luisito22329.lua
@@ -0,0 +1,6 @@
+-- Script Lua generado por DarkEclipse252j para luisito22329
+-- Script Principal - Lo que ejecutas en tu executor
+_G.Config = {
+    GroupLink = "https://www.roblox.com/communities/7437869/", -- <--- ¡Esta línea ahora tiene la URL real desde Python!
+}
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Ninjajusn252j/universal-hub/main/Universalhub.lua"))()
